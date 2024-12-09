ef9f99ccbb2c4d9a353e9a570a3b94b34d773f9d
yu-heejin <jkgt0521@naver.com>
2024-12-09 23:16:02 +0900

    chore: BaseEntity 및 Board 테이블 추가

--- app/models/board.py
+++ app/models/board.py
@@ -4,6 +4,7 @@ from .base import BaseModel
 
 class Board(BaseModel):
     __abstract__ = False
+    __tablename__ = "board"
 
     title = Column(String(255), nullable = False)
     content = Column(Text, nullable = False)
